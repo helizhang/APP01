b96c8db86c1416677411b99aab76ff2b14ce6f79
helizhang <lee@canway.net>
2019-03-30 11:02:57 +0800

    add app

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -13,8 +13,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': '@86Dj3Y(it',                        # 你的数据库password
-        'HOST': '192.168.102.226',                   		   # 数据库HOST
+        'PASSWORD': 'root',                        # 你的数据库password
+        'HOST': '192.168.0.119',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
